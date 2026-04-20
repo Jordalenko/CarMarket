7f4748537ae500ca1ba2159f0bdcfc5b499722f5
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2026-04-20 23:51:20 +0100

    Renamed media folder assets and corrected path in settings. Added sub-folders to assets folder and the db schema png

--- CarMarket/settings.py
+++ CarMarket/settings.py
@@ -166,8 +166,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/assets/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
